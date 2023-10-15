8d34b8e577165f5d68673d293f1f3f4d3772515d
MrAufa111 <mochammadrifqiaufa02@gmail.com>
2023-10-15 22:59:58 +0700

    Commit

--- application/views/billing/templates/sidebar.php
+++ application/views/billing/templates/sidebar.php
@@ -5,7 +5,7 @@
     $this->db->from('user_menu');
     $this->db->join('user_access_menu', 'user_menu.id = user_access_menu.menu_id');
     $this->db->where('user_access_menu.role_id', $role_id);
-    $this->db->order_by('user_access_menu.menu_id', 'ASC');
+    $this->db->order_by('user_menu.urut', 'ASC');
     $menu = $this->db->get()->result_array();
     ?>
     <ul class="sidebar-nav" id="sidebar-nav">
@@ -13,7 +13,7 @@
         ?>
             <?php if ($m['Active'] == 1 && $m['kategori'] == 'Billing') : ?>
                 <li class="nav-item">
-                    <a class="nav-link collapsed" data-bs-target="#<?= $m['menu'] ?>" data-bs-toggle="collapse" href="#">
+                    <a class="nav-link collapsed" data-bs-target="#_<?= $m['id'] ?>" data-bs-toggle="collapse" href="#">
 
                         <i class="<?= $m['icon'] ?>"></i><span><?= $m['menu']; ?></span>
                         <?php if (empty($m['url'])) : ?>
@@ -22,14 +22,13 @@
                     </a>
                     <?php
                     $menuId = $m['id'];
-                    $this->db->select('user_sub_menu.* , user_menu.menu');
+                    $this->db->select('user_sub_menu.* ');
                     $this->db->from('user_sub_menu');
-                    $this->db->join('user_menu', 'user_menu.id = user_sub_menu.menu_id');
                     $this->db->where('menu_id', $menuId);
                     $this->db->where('is_active', 1);
                     $subMenu = $this->db->get()->result_array();
                     foreach ($subMenu as $sm) : ?>
-                        <ul id="<?= $sm['menu']; ?>" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                        <ul id="_<?= $sm['menu_id']; ?>" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                             <li>
                                 <a href="<?= base_url($sm['url']); ?>">
                                     <i class="bi bi-circle"></i><span><?= $sm['title']; ?></span>
